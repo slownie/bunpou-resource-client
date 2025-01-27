0ce98199ff3d31e83a92cd87b428db61a9b7003a
Samuel <30960735+slownie@users.noreply.github.com>
2025-01-27 15:01:01 -0500

    changes

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -3,13 +3,14 @@ import { ref } from "vue";
 import router from "@/router";
 
 export const useUserStore = defineStore("userState", () => {
+  // State Variables
   const user = ref(null);
   if (localStorage.getItem("user") !== undefined) {
     user.value = localStorage.getItem("user");
   }
-
   const error = ref(null);
 
+  // Auth Actions
   async function signup(email, password) {
     this.error = null;
     const res = await fetch(
@@ -62,5 +63,28 @@ export const useUserStore = defineStore("userState", () => {
     router.go("/");
   }
 
-  return { user, error, signup, login, logout };
+  // Data Actions
+  async function addSentence(sentenceObject) {
+    const userID = this.user._id;
+    try {
+      const res = await fetch(
+        "https://bunpou-resource-server.vercel.app/api/users/addSentence",
+        {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ userID, sentenceObject }),
+        }
+      );
+      if (res.ok) {
+        // Add the new sentence to the pinia state
+        this.user.sentenceBank.append(sentenceObject);
+      }
+    } catch (error) {
+      this.error = error;
+    }
+  }
+
+  async function removeSentence(sentenceObject) {}
+
+  return { user, error, signup, login, logout, addSentence };
 });
