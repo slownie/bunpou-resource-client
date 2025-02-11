160ed8aeef96f056c3f3d6ca4c6c76eda5e6d33a
Samuel <30960735+slownie@users.noreply.github.com>
2025-02-11 13:20:34 -0500

    jlpt component

--- src/views/practice/JLPTPracticeView.vue
+++ src/views/practice/JLPTPracticeView.vue
@@ -1,3 +1,21 @@
+<script>
+import TheJLPTComponent from "@/components/TheJLPTComponent.vue";
+import sourceData from "@/data/jlpt.json";
+
+export default {
+  components: {
+    TheJLPTComponent,
+  },
+
+  data() {
+    return {
+      jlptQuestions: sourceData,
+    };
+  },
+};
+</script>
+
 <template>
   <h1>JLPT Practice</h1>
+  <TheJLPTComponent />
 </template>
